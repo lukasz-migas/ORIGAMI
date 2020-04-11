ea6754945ac49dd23a23cfb2dce14e57ffd01dbc
Lukasz Migas <lukas.migas@yahoo.com>
2020-04-11 13:27:51 +0200

    Added download tests

--- origami/config/download.py
+++ origami/config/download.py
@@ -5,17 +5,28 @@ import os
 # Local imports
 from origami.readers.io_json import read_json_data
 
+LINKS = {
+    "waters_raw_im": "https://www.dropbox.com/s/6un130m2c7n6fwd/WATERS_IM_SMALL.raw.zip?dl=1",
+    "text_ms": "https://www.dropbox.com/s/afgvw1r8tco27zc/TEXT_MS.zip?dl=1",
+}
+
 
 class Download:
     def __init__(self):
         path = os.path.dirname(os.path.abspath(__file__))
+        self._json_path = os.path.join(path, "download.json")
         try:
-            self.links = read_json_data(os.path.join(path, "download.json"))
+            self.links = read_json_data(self._json_path)
+            self._loaded = True
         except FileNotFoundError:
-            self.links = dict()
-            print("Failed to initilize download instance")
+            self.links = LINKS
+            self._loaded = False
+            print("Failed to initialize download instance")
 
     def __getitem__(self, item):
         if item in self.links:
             return self.links[item]
-        raise KeyError(f"Could not retrieve download link - try: {list(self.links.keys())}")
+        raise KeyError(
+            f"Could not retrieve download link - try: {list(self.links.keys())}"
+            f" [config: {self._json_path}; loaded={self._loaded}]"
+        )
---
+++ origami/config/tests/test_download.py
@@ -0,0 +1,32 @@
+import pytest
+
+from origami.config.download import Download
+import os
+
+
+class TestDownload:
+    def test_download(self):
+        dw = Download()
+        assert dw
+        assert dw._loaded is True
+        assert len(dw.links) > 0
+
+        path = dw["text_ms"]
+        assert isinstance(path, str)
+
+        with pytest.raises(KeyError):
+            _ = dw["not there"]
+
+    def test_download_diff_cwd(self):
+        path = os.path.dirname(os.getcwd())
+        os.chdir(path)
+        dw = Download()
+        assert dw
+        assert dw._loaded is True
+        assert len(dw.links) > 0
+
+        path = dw["text_ms"]
+        assert isinstance(path, str)
+
+        with pytest.raises(KeyError):
+            _ = dw["not there"]
