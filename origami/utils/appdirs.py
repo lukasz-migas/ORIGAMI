ca32c6fd24be386296e38cfd4b6652a86c51b72e
Lukasz Migas <lukas.migas@yahoo.com>
2021-01-10 16:53:46 +0100

    Update appdirs.py

--- origami/utils/appdirs.py
+++ origami/utils/appdirs.py
@@ -13,12 +13,15 @@ APP_NAME = "origami"
 APP_AUTHOR = False
 
 USER_DATA_DIR = appdirs.user_data_dir(APP_NAME, APP_AUTHOR, __version__)
+if not os.path.exists(USER_DATA_DIR):
+    make_directory(USER_DATA_DIR)
+
 USER_CACHE_DIR = os.path.join(USER_DATA_DIR, "Cache")
 USER_CONFIG_DIR = os.path.join(USER_DATA_DIR, "Configs")
 USER_LOG_DIR = os.path.join(USER_DATA_DIR, "Logs")
 USER_EXAMPLE_DIR = os.path.join(USER_DATA_DIR, "Data")
 
 # ensure all of these exist
-for path in [USER_DATA_DIR, USER_CACHE_DIR, USER_CONFIG_DIR, USER_LOG_DIR, USER_EXAMPLE_DIR]:
+for path in [USER_CACHE_DIR, USER_CONFIG_DIR, USER_LOG_DIR, USER_EXAMPLE_DIR]:
     if not os.path.exists(path):
         make_directory(path)
